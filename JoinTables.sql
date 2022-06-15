c95b799aa6bfa9e6441a1318c6fc285be5ad2dfc
MOHIT BHASKARRAO GAIKWAD <97322025+gaikwadmohit@users.noreply.github.com>
2022-06-15 14:44:58 +0530

    Added: update join three table in mysql

--- JoinTables.sql
+++ JoinTables.sql
@@ -1,57 +1,145 @@
+use spotify;
+
+create table users(useer_id int primary key,
+user_name varchar(50),
+user_email varchar(50));
+select * from users;
+
+create table playlist(playlist_id int primary key,
+playlist_name varchar(50),
+playlist_desc varchar(50),
+useer_id int,
+foreign key(useer_id) references users(useer_id));
+desc playlist;
+
+create table tracks(track_id int primary key,
+track_name varchar(50),
+track_desc varchar(50),
+playlist_id int,
+foreign key(playlist_id) references playlist(playlist_id));
+desc tracks;
+
+alter table tracks
+change useer_id
+playlist_id int;
+drop table tracks;
+
+insert into users values(1,'mohit','mohit@gmail.com'),(2,'monu','monu@gmail.com');
+insert into playlist values(100,'play1','desc1',1),(200,'play2','desc2',2);
+insert into tracks values(1000,'track1','des1',100),(2000,'track2','des2',200);
+
+select * from playlist;
+
+#--Use Alter to add column and drop column
+
+ALTER TABLE users 
+ ADD user_surname varchar(40) 
+ AFTER user_name;
+ 
+ALTER TABLE users DROP COLUMN user_surname;
+
+#--Rename,Truncate,Insert,Multiple insert
+
+#Rename
+ALTER TABLE users   
+CHANGE COLUMN user_name users_first_name varchar(25);  
+
+#Turncate
+CREATE TABLE customer (    
+    Id int PRIMARY KEY NOT NULL,     
+    Name varchar(45) NOT NULL,     
+    Product varchar(45) DEFAULT NULL,     
+    Country varchar(25) DEFAULT NULL,     
+    Year int NOT NULL    
+);    
+INSERT INTO customer ( Id, Name, Product, c, Year)     
+VALUES (1, 'Stephen', 'Computer', 'USA', 2015),     
+(2, 'Joseph', 'Laptop', 'India', 2016),     
+(3, 'John', 'TV', 'USA', 2016),    
+(4, 'Donald', 'Laptop', 'England', 2015),    
+(5, 'Joseph', 'Mobile', 'India', 2015),    
+(6, 'Peter', 'Mouse', 'England', 2016);  
+select * from customer;
+TRUNCATE  customer;  
+select * from customer;
+
+#Contrants:
+ CREATE TABLE Shirts (    
+    id INT PRIMARY KEY AUTO_INCREMENT,     
+    name VARCHAR(35),     
+    size ENUM('small', 'medium', 'large', 'x-large')    
+);  
+ CREATE TABLE ShirtBrands(Id INTEGER, BrandName VARCHAR(40) UNIQUE, Size VARCHAR(30));  
+ 
+ 
+ #Delete,Order by,Ascending,Descending,select,where
+SELECT *  
+FROM customer  
+WHERE Country ='USA'
+ORDER BY Id DESC; 
+
+#And/Or
+SELECT *  
+FROM customer  
+WHERE Name = 'Joseph'  
+AND Id > 3;  
+
+#Distinct Clause: show unique data only
+SELECT DISTINCT Country  
+FROM customer;  
+
+#Group BY---> Aggregate function sum,count,min,max
+
+#Count-->
+SELECT Country, COUNT(*)  
+FROM   Customer   
+GROUP BY Country;
+
+#Min-->
+SELECT Name, MIN(year) AS "Minimum year"  
+FROM Customer 
+GROUP BY Name;  
+#Max-->
+SELECT Name, max(year) AS "Max year"  
+FROM Customer 
+GROUP BY Name; 
+#Avg-->
+SELECT Name, avg(year) AS "Avg year"  
+FROM Customer 
+GROUP BY Name; 
+#Sum-->
+ SELECT SUM(year) AS "Total year" FROM Customer;  
 
 
-create database jointables;
-use jointables;
-
-
-CREATE TABLE members (
-    member_id INT AUTO_INCREMENT,
-    name VARCHAR(100),
-    PRIMARY KEY (member_id)
-);
-
-CREATE TABLE committees (
-    committee_id INT AUTO_INCREMENT,
-    name VARCHAR(100),
-    PRIMARY KEY (committee_id)
-);
-
-INSERT INTO members(name)
-VALUES('John'),('Jane'),('Mary'),('David'),('Amelia');
-
-INSERT INTO committees(name)
-VALUES('John'),('Mary'),('Amelia'),('Joe');
-
-
-SELECT * FROM members;
-SELECT * FROM committees;
-
-SELECT * from members
-INNER JOIN committees ON
-committee_id=member_id ;
-
-
-SELECT * FROM members;
-SELECT * FROM committees;
-
-SELECT * from members
-left JOIN committees ON
-committee_id=member_id ;
-
-
-SELECT * FROM members;
-SELECT * FROM committees;
-
-SELECT * from members
-right JOIN committees ON
-committee_id=member_id ;
 
 
 
 
-SELECT members.member_id,committees.committee_id from members
-cross JOIN committees;
 
 
-SELECT * FROM members;
-SELECT * FROM committees;
+select * from users
+join playlist on 
+users.useer_id = playlist.useer_id;
+
+
+
+select * from users
+join tracks on 
+users.useer_id = tracks.useer_id;
+
+
+
+select users.useer_id,playlist.playlist_id from users
+join playlist on 
+users.useer_id = playlist.useer_id;
+
+select users.useer_id,tracks.track_id from users
+join tracks on 
+users.useer_id = tracks.playlist_id;
+
+SELECT users.useer_id,playlist.playlist_id,tracks.playlist_id
+  FROM users 
+  JOIN playlist ON users.useer_id = playlist.useer_id
+  JOIN tracks ON playlist.playlist_id= tracks.playlist_id;
+ 
+
